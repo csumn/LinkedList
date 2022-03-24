808fe560340b4b9a20b3246bd4e0d8aa31d27914
csumn <csumn13@gmail.com>
2022-03-24 16:13:54 +0530

    UC 2 added

--- LinkedList/src/com/bridgelabz/Main.java
+++ LinkedList/src/com/bridgelabz/Main.java
@@ -2,11 +2,10 @@ package com.bridgelabz;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to linked list program");
-		LinkedList list = new LinkedList();
-		list.push(70);
-		list.push(30);
-		list.push(56);
-		list.display();
+		LinkedList linkedList = new LinkedList();
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+		linkedList.print();
 	}
 }
