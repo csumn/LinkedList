d00203e65169bc059ad965e52fdfedef05ee7e97
csumn <csumn13@gmail.com>
2022-03-24 15:38:48 +0530

    simple Linked List of 56, 30 and 70

---
+++ LinkedList/src/com/bridgelabz/Main.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to linked list program");
+		LinkedList list = new LinkedList();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		list.display();
+	}
+}
